cbaf7fe99e65497730a1549dae6c43d5229a6216
Shivangi97sahni <40377371+Shivangi97sahni@users.noreply.github.com>
2018-07-08 13:00:07 +0530

    Add files via upload

---
+++ assgn.py
@@ -0,0 +1,20 @@
+import numpy as np
+a = np.array ([[2.46869],
+               [4.57689],
+                [3.6809],
+                [1.577686],
+                  [0.5809],
+                  [5.79999],
+                   [0.3456],
+                    [3.780],
+                  [0.476734],
+                  [0.45567]])
+
+print("Our array is : ")
+print(a)
+print("\n")
+
+print("Applying mean() function: ")
+print(np.mean(a))
+print("\n")
+
